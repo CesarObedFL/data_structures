c3acffba1919d455a890d56d12cc8548e81cd9a3
Cesar Figueroa <cofigueroapc@corporativosade.com.mx>
2023-07-21 12:08:11 -0600

    [INIT]

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <climits>
+#include <cfloat>
+
+#ifndef _Linux
+	#define Limpiar system("clear");
+#elif _WIN32
+	#define Limpiar system("cls");
+#elif _WIN64
+	#define Limpiar system("cls");
+#endif
+
+#include "Lista.h"
+
+using namespace std;
+
+void pausa(){ cout<<"\nPulse una tecla para continuar..."; cin.get(); }
+
+int main() {
+    Limpiar;
+    cout << "\n === Tipos de Datos Primitivos === \n" << endl;
+    cout << "Tipo de Dato" << setw (20) << "Bits" << setw (25) << "Valor Minimo" << setw (25) << "Valor Maximo\n" << endl;
+    cout << "Caracter con signo" << setw (14) << sizeof(char)*8 << setw (25) << CHAR_MIN << setw (25) << CHAR_MAX << endl;
+    cout << "Caracter sin signo" << setw (14) << sizeof(unsigned char)*8 << setw (25) << 0 << setw (25) << UCHAR_MAX << endl;
+    cout << "Entero corto con signo" << setw (10) << sizeof(short)*8 << setw (25) << SHRT_MIN << setw (25) << SHRT_MAX << endl;
+    cout << "Entero corto sin signo" << setw (10) << sizeof(unsigned short)*8 << setw (25) << 0 << setw (25) << USHRT_MAX << endl;
+    cout << "Entero largo con signo" << setw (10) << sizeof(long)*8 << setw (25) << LONG_MIN << setw (25) << LONG_MAX << endl;
+    cout << "Entero largo sin signo" << setw (10) << sizeof(unsigned long)*8 << setw (25) << 0 << setw (25) << ULONG_MAX << endl;
+    cout << "Real de precision simple" << setw (8) << sizeof(float)*8 << setw (25) << FLT_MIN << setw (25) << FLT_MAX << endl;
+    cout << "Real de precision doble" << setw (9) << sizeof(double)*8 << setw (25) << DBL_MIN << setw (25) << DBL_MAX << endl;
+    cout << "\n";
+    pausa();
+
+    return 0;
+}
